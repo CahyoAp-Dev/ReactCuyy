c3261f2c50c926df67fc988780a81cbdd96d2e68
Cahyo AP <cahyoadiprasetia@gmail.com>
2025-05-16 14:54:51 +0700

    feat: Example of pure-component

--- hello-world/main.jsx
+++ hello-world/main.jsx
@@ -4,6 +4,7 @@ import Container from "../nested-component/Container.jsx";
 import CollectionTodoList from "../collection-component/CollectionTodoList.jsx";
 import ConditionalTodoList from "../conditional/ConditionalTodoList.jsx";
 import Props from "../props/Props.jsx";
+import Table from "../pure-component/Table.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -11,6 +12,7 @@ createRoot(document.getElementById("root")).render(
       <Props />
       <CollectionTodoList />
       <ConditionalTodoList />
+      <Table />
     </Container>
   </StrictMode>
 );
