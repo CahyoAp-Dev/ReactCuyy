a60ed9cf4020d6735b0c4649e35906908516d899
Cahyo AP <cahyoadiprasetia@gmail.com>
2025-05-16 10:54:23 +0700

    feat: Example of conditional with ternary operator and null component

--- conditional/Todo.jsx
+++ conditional/Todo.jsx
@@ -1,11 +1,11 @@
-export default function Todo({ text, isCompleted }) {
-  if (isCompleted) {
-    return (
-      <li>
-        <del>{text}</del>
-      </li>
-    );
-  } else {
-    return <li>{text}</li>;
-  }
+export default function Todo({ text, isCompleted, isDeleted }) {
+    if (isDeleted) {
+        return null;
+    } else {
+        return (
+            <li>
+                {isCompleted ? <del>{text}</del> : text}
+            </li>
+        );
+    }
 }
--- conditional/TodoList.jsx
+++ conditional/TodoList.jsx
@@ -3,7 +3,8 @@ import Todo from "./Todo.jsx";
 export default function TodoList() {
     return (
         <ul>
-            <Todo text="Belajar React" isCompleted={true} />
+            <Todo text="Belajar React" isCompleted={true} isDeleted={true} />
+            <Todo text="Belajar React 2025" isCompleted={true} />
             <Todo text="Belajar React Native" isCompleted={false} />
         </ul>
     );
